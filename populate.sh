588c43af048e596e9dc9dad3e70f74f2e77e9a1a
willie <willief@gmail.com>
2023-01-23 08:36:30 +0000

    Could not deserialize specified hex string to a DBC: Odd number of digits

--- populate.sh
+++ populate.sh
@@ -6,26 +6,31 @@
 
 set_env_vars () {
     #export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4317" # Already the default
-    export RUST_LOG=sn_node=info # This filters the output for stdout/files, not OTLP
+    export RUST_LOG=sn_node=info, sn_cli=trace # This filters the output for stdout/files, not OTLP
     #export RUST_LOG_OTLP=sn_node=trace # This filters what is sent to OTLP endpoint 
     export SAFE_ROOT=$HOME/.safe
     export SAFE_BIN=/usr/local/bin
     export TESTNET_NAME=baby-fleming
     export WALLET_DATA=$SAFE_ROOT/testcreds.txt
     export ACCTS=$SAFE_ROOT/accounts/
+    echo "done set_env_vars"
 }
 
 
 check_stash () {
   cd $SAFE_ROOT
   STASH=$(cat $WALLET_DATA)
-  echo $STASH
+  echo "done check Stash" $STASH
 }
 
 init_accts () {
-  [ ! -d "$ACCTS" ] && mkdir $ACCTS  
+  #[ ! -d "$ACCTS" ] && mkdir $ACCTS  
   cd $ACCTS
-  rm -v *.txt 
+  rm -v *.txt
+  sleep 2
+  ls -l . 
+  pwd
+  sleep 5
 }
 
 get_accts_qty () {
@@ -37,13 +42,14 @@ get_accts_qty () {
         echo "OK"
         break
         else
-        echo "Choose between 1 and 500 nodes"
+        echo "Choose between 1 and 500 wallets"
         fi
     done
 }
 
 create_accts () {
-  for i in {1..5}
+  cd $ACCTS
+  for i in {113..114}
   do 
     touch account_$i.txt
     acct=$($SAFE_BIN/safe nrs register --json account_$i |jq '.[0]')
@@ -62,12 +68,14 @@ create_accts () {
 }
 
 payout () {
+  #cd $ACCTS
+  ls -l .
   for files in $ACCTS/*.txt
   do
-    #nrs_acct=$( head -n 1 $files)
+    nrs_acct=$( head -n 1 $files)
     pubk=$( tail -n 2 $files | head -n1)
     wurl=$( tail -n 1 $files)
-    echo "----------------------------------------------------"
+    echo "---------- print  nrs_acct, pubk, wurl ---------------"
     echo $nrs_acct
     echo $pubk
     wurl=$(echo $wurl| sed 's/^\"\|\"$//g')
@@ -75,8 +83,10 @@ payout () {
 
     #everyone should get a slice of pi
     $SAFE_BIN/safe wallet reissue --json --from $STASH 3.1415922 > ./dbc
+    ls -l >./dbc
     $SAFE_BIN/safe wallet deposit --json --dbc ./dbc $wurl 
-    $SAFE_BIN/safe wallet balance $STASH
+    echo $($SAFE_BIN/safe wallet balance $STASH)
+    echo "----------------------------------------------------"
     $SAFE_BIN/safe wallet balance $wurl
     echo "----------------------------------------------------"
 done  
@@ -84,8 +94,12 @@ done
 
 set_env_vars
 check_stash
+init_accts
+sleep 3
 get_accts_qty
+sleep 3
 create_accts
+sleep 3
 payout
 
 exit 0
